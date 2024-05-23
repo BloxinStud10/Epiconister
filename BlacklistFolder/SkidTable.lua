5c9f937abe9aad7568b21990d6a78f49d8352c96
pepfre12 <140227723+pepfre12@users.noreply.github.com>
2024-05-23 22:34:06 +0800

    Update SkidTable.lua

--- BlacklistFolder/SkidTable.lua
+++ BlacklistFolder/SkidTable.lua
@@ -2,4 +2,5 @@ return {
   ["Juniorek36"] = 866465234, --Attempt to dox Genesis FE over Elio blasio script, threatening to box me lol cry bout it
   ["adam3rd"] = 248908110, --Reusing other converted scripts, steal walk animate (in john doe scr) from Maxilito's Star Glitcher, Calling his friends over anything cuz damn knowing he cooked, paid bucks to Typh on Ko-fi???? nahhhhh. Overall MelonSkidder
   ["Cherries_Rose"] = 5164112124 --Same to 2rd reason
+  ["Pizza_alt368"] = 5768260349 --Flinging my ass
 }
