224417cd974b9e0bb553063582b0c3ef20f0d0a8
DangerRedile <101493158+BloxinStud10@users.noreply.github.com>
2024-08-13 23:04:22 +0700

    Update SkidTable.lua

--- BlacklistFolder/SkidTable.lua
+++ BlacklistFolder/SkidTable.lua
@@ -1,6 +1,6 @@
 return {
     [{Id=1,Name="Roblox"}] = "what the sigma",
-    [{Id=866465234,Name="Juniorek36"}] = "Cry about it and fuck your life",
+    [{Id=866465234,Name="Juniorek36"}] = "Cry about it and fuck your life (attempt doxxing)",
     [{Id=248908110,Name="adam3rd"}] = "Got ur Facecam, hand up skid",
     [{Id=5164112124,Name="Cherries_Rose"}] = "Got ur Facecam, hand up skid",
     [{Id=5099504885,Name="MeIonScripter"}] = "Got ur Facecam, hand up skid",
